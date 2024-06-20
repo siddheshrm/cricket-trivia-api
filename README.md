9734746e695b2ca34cf471102c0b17f7d21728f3
siddheshrm <siddheshmestri07@gmail.com>
2024-06-21 00:19:28 +0530

    my own api

---
+++ README.md
@@ -0,0 +1,15 @@
+# npm init
+
+# in package.json, add "type": "module" 
+
+# npm i express
+
+# npm install cors
+
+# in package.json, add "engines": {"node": "20.11.1"} after dependencies
+
+# npm install -g heroku (to install heroku first time)
+
+# heroku login, and open in browser
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+
+// To handle ES modules and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Load data from JSON file
+const apiData = JSON.parse(readFileSync(path.join(__dirname, 'data.json'), 'utf-8'));
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/cricket_trivia", (req, res) => {
+    res.send(apiData);
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}.`);
+});
